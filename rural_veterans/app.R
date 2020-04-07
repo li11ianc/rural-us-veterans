2cc14a2568a2be0b971c44bdfbf98326ed1db991
Lillian Clark <lmc97@duke.edu>
2020-04-06 23:32:12 -0700

    first plot added to shiny

--- rural_veterans/app.R
+++ rural_veterans/app.R
@@ -1,15 +1,47 @@
 library(shiny)
+library(tidyverse)
+library(maps)
+library(shinythemes)
+
+theme_custom <- function() {
+  theme_bw() +
+    theme(
+      axis.title = element_text(size = 16), 
+      title = element_text(size = 20),
+      legend.title = element_text(size = 10),
+      legend.text = element_text(size = 10),
+      axis.text.x = element_text(size = 12),
+      axis.text.y = element_text(size = 12),
+      plot.caption = element_text(size = 10))
+}
 
 
-ui <- fluidPage(
-
-
-)
-
+ui <- fluidPage(theme = shinytheme("yeti"),
+                textInput(inputId = "demo",
+                              label   = "Rural/Urban/Total",
+                              value   = "Rural"),
+                plotOutput(outputId = "plot"))
 
 
 server <- function(input, output) {
+  output$plot <- renderPlot({
+    
+    x_map <- vets_map %>%
+      filter(rural_urban == input$demo, state != "Alaska", state != "Hawaii")
 
+    us_map <- map_data("state")
+    
+    # create visualization
+    cols <- c("Rural" = "#F4200B", "Urban" = "#F4D03F", "Total" = "#8E44AD")
+    
+    ggplot() + 
+      geom_polygon(data = us_map, aes(x=long, y=lat, group=group),
+                   color="#85C1E9", fill = "#EBF5FB") +
+      geom_point(data = x_map, aes(x = x, y = y, size = total, color = rural_urban, alpha = .7))  +
+      scale_size_continuous(range=c(1,15)) +
+      scale_color_manual(values = cols) +
+      coord_map()
+  })
 
 }
 
